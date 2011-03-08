c84520017108bcea740c783e82d16b37c02da64a
Raymond Ko <rkokim@gmail.com>
2011-03-08 12:49:20 +0800

    Disable encoding in environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Japify::Application.initialize!
-Encoding.default_internal = Encoding.find("UTF-8")
+# Encoding.default_internal = Encoding.find("UTF-8")
