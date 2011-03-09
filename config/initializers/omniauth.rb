8ff30417daeff9ac15272c1938160db4989a0a6f
sw0rd <rkokim@gmail.com>
2011-03-08 19:05:33 -0800

    Edited config/initializers/omniauth.rb via GitHub

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
-GOOGLE_API = 'ABQIAAAA50aVEQhwqSIbzfgbTlQ4SxSXc6Nw8u9opE6A42eZAo88N8mmfRQd4mSOX751tBvEXJfvUq84yLC1KQ'
-YAHOO_API = 'PmEKFgSxg64vTpu_bXC9k8xwnbQQLrkAoLb3IUTSSK3yYyHGX6EubDN3ITk65HR5Negmbh.c'
+GOOGLE_API = 'x'
+YAHOO_API = 'x'
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '4tuML4R739WakcXq4YEdfw', 'lywwouBAyLzgCJEuz1MkLzMaILsJCjhuWpL8BugCbs'
-  provider :facebook, '189836181047137', '1e9a8c249eef296d27c7c443fa6e3f07'
+  provider :twitter, '4x', 'lxBugCbs'
+  provider :facebook, '18x37', '1e9xfa6e3f07'
 end
 
